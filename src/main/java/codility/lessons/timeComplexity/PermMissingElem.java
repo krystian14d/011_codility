38d9137080629e73c72d22656780bba2be22bffb
Krystian <krystian14d@gmail.com>
2022-04-16 14:22:27 +0200

    PermMissingElement task

--- src/main/java/codility/lessons/timeComplexity/PermMissingElem.java
+++ src/main/java/codility/lessons/timeComplexity/PermMissingElem.java
@@ -1,7 +1,27 @@
 package codility.lessons.timeComplexity;
 
+import java.util.HashSet;
+
 public class PermMissingElem {
     public static void main(String[] args) {
 
+        int[] A = {2, 3, 5, 1};
+        System.out.println("Missing element is " + solution(A));
+    }
+
+    static int solution(int[] A) {
+
+        HashSet<Integer> numbers = new HashSet<>();
+        int maxNumber = A.length + 1;
+
+        for (int i = 0; i < A.length; i++) {
+            numbers.add(A[i]);
+        }
+
+        for (int i = 1; i < maxNumber + 1; i++) {
+            if (!numbers.contains(i))
+                return i;
+        }
+        return 0;
     }
 }
