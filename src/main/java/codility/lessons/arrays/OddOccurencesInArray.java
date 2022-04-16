d255f75f2978418c7c41934d04c741df21d5b917
Krystian <krystian14d@gmail.com>
2022-04-16 13:51:28 +0200

    Odd occurences in Array

--- src/main/java/codility/lessons/arrays/OddOccurencesInArray.java
+++ src/main/java/codility/lessons/arrays/OddOccurencesInArray.java
@@ -4,14 +4,22 @@ import java.util.HashSet;
 
 public class OddOccurencesInArray {
     public static void main(String[] args) {
-
+        System.out.println(solution(new int[]{9, 6, 2, 3, 7, 9, 7, 9, 6, 1, 1}));
     }
 
-    public static int solution(int[] A){
-        HashSet<Integer> duplicates = new HashSet<>();
-        int element = -1;
-        for (int i = 0; i < A.length; i++) {
-            
+    public static int solution(int[] A) {
+        HashSet<Integer> ints = new HashSet<>();
+        for (int a : A) {
+            if (!ints.contains(a)) {
+                ints.add(a);
+            } else {
+                ints.remove(a);
+            }
+
         }
+        for (Integer a : ints) {
+            return a;
+        }
+        return 0;
     }
 }
