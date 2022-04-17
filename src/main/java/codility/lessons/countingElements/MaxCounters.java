23cc14743634a0056dfb58b25ddb29853ec381ea
Krystian <krystian14d@gmail.com>
2022-04-17 16:26:31 +0200

    MaxCounters task start

---
+++ src/main/java/codility/lessons/countingElements/MaxCounters.java
@@ -0,0 +1,11 @@
+package codility.lessons.countingElements;
+
+public class MaxCounters {
+    public static void main(String[] args) {
+
+    }
+
+    static int solution (int N, int[] A){
+        return 0;
+    }
+}
