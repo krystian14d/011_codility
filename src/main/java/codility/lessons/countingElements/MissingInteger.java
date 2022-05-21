c837c1ff4ea9cdeb0e0c8345c0807afce1aba0c9
Krystian <krystian14d@gmail.com>
2022-05-21 17:47:04 +0200

    missing integer task solution

--- src/main/java/codility/lessons/countingElements/MissingInteger.java
+++ src/main/java/codility/lessons/countingElements/MissingInteger.java
@@ -3,14 +3,16 @@ package codility.lessons.countingElements;
 public class MissingInteger {
 
     public static void main(String[] args) {
-
+        int[] A = {1, 3, 6, 4, 1, 2};
+        System.out.println(solution(A));
     }
 
-    public int solution(int[] A) {
+    public static int solution(int[] A) {
 
         boolean[] existingNumbers = new boolean[100001];
 
         for (int i : A) {
+            System.out.println(i);
             if (i >= 1 && i <= 100000) {
                 existingNumbers[i - 1] = true;
             }
